47745bd46f2a5a8d7355e860bd08e63288b03204
apichat <apichathot@gmail.com>
2024-06-07 16:27:25 +0700

    ng update @angular/core@15 @angular/cli@15

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -233,7 +233,7 @@ const routes: Routes = [
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
+    RouterModule.forRoot(routes, {}),
     SharedModule,
     FormsModule,
     CommonModule,
