fe91a94eaacaccaef8bc6ccf80662918b967bee8
pornarong <pornarong@gmail.com>
2024-06-11 13:24:47 +0700

    update http interceptor

--- src/app/interceptor/httperror.interceptor.ts
+++ src/app/interceptor/httperror.interceptor.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { Observable, from, throwError } from 'rxjs';
+import { catchError, mergeMap, tap } from 'rxjs/operators';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { Router } from '@angular/router';
 import TokenUtils from '../shared/token-utils';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { environment } from 'src/environments/environment';
+
+const IGNORE_LOADING = [
+
+];
+const IGNORE_AUTH_LIST = [
+
+];
 
 @Injectable()
 export class HttpErrorInterceptor implements HttpInterceptor {
@@ -17,35 +25,67 @@ export class HttpErrorInterceptor implements HttpInterceptor {
   ) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    this.showLoader();
-    return next.handle(request).pipe(tap((event: HttpEvent<any>) => {
-      if (event instanceof HttpResponse) {
-        this.onEnd();
-      }
-    }), catchError(err => {
-      this.onEnd();
-      if (err.status === 0 || err.status === 401 || err.status === 403) {
+    return from(this.checkToken(request.url)).pipe(
+      mergeMap(() => {
+        if (!this.checkIgnoreLoading(request.url)) {
+          return from(this.spinner.show()).pipe(
+            mergeMap(() => {
+              return this.handleAccess(request, next);
+            })
+          );
+        } else {
+          return this.handleAccess(request, next);
+        }
+      })
+    );
+  }
+
+  private handleAccess(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      tap((event: HttpEvent<any>) => {
+        if (event instanceof HttpResponse) {
+          if (!this.checkIgnoreLoading(request.url)) {
+            this.spinner.hide();
+          }
+        }
+      }),
+      catchError((err) => {
+        this.spinner.hide();
+        if (err.status === 401) {
+          TokenUtils.removeToken();
+          this.router.navigate(['/login'], { queryParams: {} });
+        }
+        const error = err.error.message || err.statusText;
+        return throwError(error);
+      })
+    );
+  }
+
+  private async checkToken(url: string) {
+    const token = TokenUtils.getToken();
+    if (!this.checkIgnoreAuth(url)) {
+      if (token && this.jwtHelper.isTokenExpired(token, 5)) {
         TokenUtils.removeToken();
         this.router.navigate(['/login'], { queryParams: {} });
       }
-
-      // const error = err.error.message || err.statusText;
-      // console.log(err.error);
-      return throwError(err.error);
-    }));
+    }
   }
 
-  private onEnd(): void {
-    this.hideLoader();
+  private checkIgnoreAuth(url: string) {
+    if (url.indexOf(environment.endpoint) > -1) {
+      const path = url.replace(environment.endpoint, "");
+      return IGNORE_AUTH_LIST.find((item) => path.indexOf(item) > -1) != null;
+    } else {
+      return true;
+    }
   }
 
-  private showLoader(): void {
-    setTimeout(() => {
-      this.spinner.show();
-    }, 1);
-  }
-
-  private hideLoader(): void {
-    this.spinner.hide();
+  private checkIgnoreLoading(url: string) {
+    if (url.indexOf(environment.endpoint) > -1) {
+      const path = url.replace(environment.endpoint, "");
+      return IGNORE_LOADING.find((item) => path.indexOf(item) > -1) != null;
+    } else {
+      return true;
+    }
   }
 }
