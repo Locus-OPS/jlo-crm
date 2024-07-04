b25af966793e7942cac5f108a8170945a2938705
pornarong <pornarong@gmail.com>
2024-07-04 20:33:17 +0700

    update backend path

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // endpoint: 'http://192.168.10.177:8080/JLOXG',
-  endpoint: 'https://jlo.locus.co.th/JLOXG',
+  endpoint: 'https://jlo.locus.co.th/jlo-crm-backend',
   whitelistedDomains: ['jlo.locus.co.th']
 };
