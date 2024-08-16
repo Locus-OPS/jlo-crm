1bbf8422ea44cbce3b257e2f2193acce3e53cf98
apichat <apichathot@gmail.com>
2024-08-16 17:54:39 +0700

    questionnaire

--- src/app/pages/system/questionnaire/questionnaire.component.ts
+++ src/app/pages/system/questionnaire/questionnaire.component.ts
@@ -2,12 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { SharedModule } from 'src/app/shared/module/shared.module';
 import { CreatedByComponent } from '../../common/created-by/created-by.component';
 import { BaseComponent } from 'src/app/shared/base.component';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Dropdown } from 'src/app/model/dropdown.model';
 import { TableControl } from 'src/app/shared/table-control';
-import { QuestionnaireModel } from './questionnaire.model';
+import { QuestionnaireHeaderModel } from './questionnaire.model';
 import { ApiService } from 'src/app/services/api.service';
-import { QuestionnareService } from './questionnare.service';
+import { QuestionnaireService } from './questionnaire.service';
 import { Globals } from 'src/app/shared/globals';
 import { Router } from '@angular/router';
 
@@ -24,26 +24,52 @@ export class QuestionnaireComponent extends BaseComponent implements OnInit {
   createForm: FormGroup;
 
   statusList: Dropdown[];
+  questionnaireTypeList: Dropdown[];
+
   tableControl: TableControl = new TableControl(() => { this.search(); });
 
-  selectedRow: QuestionnaireModel;
-  dataSource: QuestionnaireModel[];
+  selectedRow: QuestionnaireHeaderModel;
+  dataSource: QuestionnaireHeaderModel[];
   displayedColumns: string[] = ['id', 'buName', 'activeYn'];
 
   constructor(
     public api: ApiService,
     private formBuilder: FormBuilder,
-    private questionnareService: QuestionnareService,
+    private questionnaireService: QuestionnaireService,
     public router: Router,
     public globals: Globals
   ) {
     super(router, globals);
-    api.getCodebookByCodeType({ data: 'ACTIVE_FLAG' })
-      .then(result => { this.statusList = result.data; });
+
+    api.getMultipleCodebookByCodeType({
+      data: ['ACTIVE_FLAG', 'QUESTIONNAIRE_TYPE']
+    }).then(result => {
+      this.statusList = result.data['ACTIVE_FLAG'];
+      this.questionnaireTypeList = result.data['QUESTIONNAIRE_TYPE'];
+    });
   }
 
   ngOnInit(): void {
+    this.searchForm = this.formBuilder.group({
+      formname: [''],
+      activeYn: [null],
+      questionnaireType: [null],
+    });
 
+    this.createForm = this.formBuilder.group({
+      id: [null],
+      formname: ['', [Validators.required, Validators.maxLength(100)]],
+      activeYn: ['N'],
+      questionnaireType: [''],
+
+      createdBy: [''],
+      createdDate: [''],
+      updatedBy: [''],
+      updatedDate: [''],
+      updatedByName: [''],
+      createdByName: ['']
+    });
+    this.search();
   }
 
   onSearch() {
@@ -51,6 +77,10 @@ export class QuestionnaireComponent extends BaseComponent implements OnInit {
   }
 
   search() {
+
+  }
+
+  onQuestionnaireCreate() {
 
   }
 
