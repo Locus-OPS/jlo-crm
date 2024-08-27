d09a5b457f5415921b30304075dd53b00bc0909f
SWx91 <sathapornx1991@gmail.com>
2024-08-27 15:26:41 +0700

    Update Questonnaire

--- src/app/pages/system/questionnaire/landing-page/landing-page.component.ts
+++ src/app/pages/system/questionnaire/landing-page/landing-page.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DomSanitizer, Title } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
 import { BaseComponent } from '@fullcalendar/core/internal';
@@ -10,11 +10,12 @@ import { SharedModule } from 'src/app/shared/module/shared.module';
 import { LandingPageService } from './landing-page.service';
 import { ApiResponse } from 'src/app/model/api-response.model';
 import Utils from 'src/app/shared/utils';
+import { MatRadioModule } from '@angular/material/radio';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [SharedModule],
+  imports: [SharedModule, MatRadioModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
@@ -22,6 +23,10 @@ export class LandingPageComponent implements OnInit {
   hasKey: string;
   headerId: number;
   expiredLink: string;
+  headerQuestionnaire: any;
+  respondentForm: FormGroup;
+  answerForm: FormGroup;
+  questionnaireQuestionList: any[];
 
   constructor(
     //public router: Router,
@@ -30,6 +35,7 @@ export class LandingPageComponent implements OnInit {
     private formBuilder: FormBuilder,
     private sanitized: DomSanitizer,
     private landingPageService: LandingPageService,
+    private el: ElementRef
     //private titleService: Title,
   ) {
     // this.titleService.setTitle('Jlo CRM : Locus telecom');
@@ -38,8 +44,19 @@ export class LandingPageComponent implements OnInit {
   ngOnInit(): void {
     this.hasKey = this.route.snapshot.paramMap.get('key');
     this.expiredLink = null;
-
+    this.createRespondentForm();
     this.getLandingQuestionnaireMaster();
+  }
+
+  createRespondentForm() {
+    this.respondentForm = this.formBuilder.group({
+      respondentId: [],
+      questionnaireHeaderId: [0, Validators.required],
+      name: [null, Validators.required],
+      age: [null, Validators.required],
+      gender: [null, Validators.required],
+      location: [null]
+    });
   }
 
   getLandingQuestionnaireMaster() {
@@ -48,8 +65,13 @@ export class LandingPageComponent implements OnInit {
     this.landingPageService.getLandingQuestionnaireMaster(params).then((result: any) => {
       if (result.status) {
         if (result.data) {
-          console.log(result.data);
-          this.expiredLink = "";
+          this.headerQuestionnaire = result.data;
+          this.questionnaireQuestionList = result.data.questionnaireList;
+          this.respondentForm.patchValue({ questionnaireHeaderId: result.data.id });
+          this.headerId = result.data.id;
+          this.createAnswerForm();
+          // console.log(result.data);
+          // this.expiredLink = "";
         } else {
           Utils.alertError({
             text: "Data not found",
@@ -63,8 +85,6 @@ export class LandingPageComponent implements OnInit {
       }
 
     }, (err: any) => {
-
-
       Utils.alertError({
         text: err.message,
       });
@@ -72,6 +92,94 @@ export class LandingPageComponent implements OnInit {
 
     );
 
+  }
+
+  createAnswerForm() {
+    const group = {};
+    this.questionnaireQuestionList.forEach(q => {
+      group[q.id] = ['', q.requiredFlg ? Validators.required : null];
+    });
+    this.answerForm = this.formBuilder.group(group);
+  }
+
+  checkValid() {
+    this.respondentForm
+  }
+
+
+  onSubmitQuestionnaire() {
+
+    //Check Form respondentForm
+    if (!this.respondentForm.valid) {
+      this.respondentForm.markAllAsTouched();
+      for (const key of Object.keys(this.respondentForm.controls)) {
+        if (this.respondentForm.controls[key].invalid) {
+          const invalidControl = this.el.nativeElement.querySelector('[formcontrolname="' + key + '"]');
+          invalidControl.focus();
+          break;
+        }
+      }
+      return;
+    }
+
+    //
+    if (!this.answerForm.valid) {
+      this.answerForm.markAllAsTouched();
+      for (const key of Object.keys(this.answerForm.controls)) {
+        if (this.answerForm.controls[key].invalid) {
+          const invalidControl = this.el.nativeElement.querySelector('[formcontrolname="' + key + '"]');
+          invalidControl.focus();
+          break;
+        }
+      }
+      return;
+    }
+
+    console.log(JSON.stringify(this.respondentForm.getRawValue()));
+    console.log(JSON.stringify(this.answerForm.getRawValue()));
+
+    const respondent = this.respondentForm.getRawValue();
+    const responseList = Object.entries(this.answerForm.getRawValue()).map(([key, value]) => ({ "responseText": (value).toString(), "questionId": Number(key), "questionnaireHeaderId": this.headerId }));
+
+    Utils.confirm('Are you sure?', 'Do you want to proceed?', 'Yes')
+      .then((result) => {
+        if (result.isConfirmed) {
+          this.landingPageService.createQuestionnaire({ data: { respodent: respondent, responses: responseList } }).then((res) => {
+            if (res.status) {
+              setTimeout(() => {
+                location.reload(); // หรือใช้ window.location.reload();
+              }, 5000);
+              Utils.alertSuccess({ text: "Successfully submitted the answer." });
+            } else {
+              Utils.alertError({ text: res.message });
+            }
+          });
+        } else {
+          console.log('Cancelled');
+        }
+      });
+
+  }
+
+  hasRequiredError(controlName: string): boolean {
+    const control = this.respondentForm.get(controlName);
+    return control?.hasError('required') && control?.touched;
+  }
+
+  responseFormhasRequiredError(controlName: string): boolean {
+    const control = this.answerForm.get(controlName);
+    return control?.hasError('required') && control?.touched;
+  }
+
+  checkboxConcatValue(param: any, formName: any) {
+    let allValue = this.answerForm.get([formName]).value;
+    let isHave = allValue.includes(param);
+    if (isHave) {
+      allValue = allValue.replace(param, "");
+    } else {
+      allValue = (allValue + " " + param).trim();
+    }
+    this.answerForm.patchValue({ [formName]: allValue });
   }
 
 }
