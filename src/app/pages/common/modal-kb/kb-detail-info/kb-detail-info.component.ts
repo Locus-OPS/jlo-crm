d0a9941bf025e37cd4dbf839c2016004ac0f9da8
SWx91 <sathapornx1991@gmail.com>
2024-09-10 16:53:54 +0700

    Update REF KB

---
+++ src/app/pages/common/modal-kb/kb-detail-info/kb-detail-info.component.ts
@@ -0,0 +1,78 @@
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { KbDetail, KbDetailInfo } from 'src/app/pages/kb/kb.model';
+import { KbService } from 'src/app/pages/kb/kb.service';
+import { KbStore } from 'src/app/pages/kb/kb.store';
+import { BaseComponent } from 'src/app/shared/base.component';
+import { Globals } from 'src/app/shared/globals';
+import { SharedModule } from 'src/app/shared/module/shared.module';
+import Utils from 'src/app/shared/utils';
+
+@Component({
+  selector: 'modal-kb-detail-info',
+  standalone: true,
+  imports: [SharedModule],
+  templateUrl: './kb-detail-info.component.html',
+  styleUrl: './kb-detail-info.component.scss'
+})
+export class KbDetailInfoComponent extends BaseComponent implements OnInit, OnDestroy {
+  @Input()
+  translatePrefix: string;
+
+  createForm: UntypedFormGroup;
+  kbDetailSubscription: Subscription;
+  kbDetail: KbDetail = null;
+
+  constructor(
+    private formBuilder: UntypedFormBuilder,
+    private kbService: KbService,
+    private kbStore: KbStore,
+    public router: Router,
+    public globals: Globals
+  ) {
+    super(router, globals);
+    this.kbDetailSubscription = this.kbStore.observeKbDetail().subscribe(detail => {
+      if (detail && detail.contentId) {
+        this.createForm.enable();
+        this.kbDetail = detail;
+        this.kbService.getKbDetailInfoById({
+          data: detail.contentId
+        }).then(result => {
+          if (result.status) {
+            this.updateFormValue(result.data);
+          }
+        });
+      } else {
+        this.kbDetail = null;
+        this.createForm.reset();
+      }
+    });
+  }
+  ngOnDestroy(): void {
+    this.kbDetailSubscription.unsubscribe();
+  }
+
+  ngOnInit() {
+    this.createForm = this.formBuilder.group({
+      contentId: [''],
+      url: [''],
+      description: ['']
+    });
+
+    this.CHECK_FORM_PERMISSION(this.createForm);
+  }
+
+  clearForm() {
+    this.createForm.reset();
+  }
+
+  updateFormValue(kbDetailInfo: KbDetailInfo) {
+    this.createForm.patchValue({
+      ...kbDetailInfo
+    });
+  }
+
+
+}
---
+++ src/app/pages/common/modal-kb/modal-kb.component.ts
@@ -0,0 +1,102 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/app/services/api.service';
+import { BaseComponent } from 'src/app/shared/base.component';
+import { Globals } from 'src/app/shared/globals';
+import { SharedModule } from 'src/app/shared/module/shared.module';
+import { TreeComponent } from '../../kb/component/tree/tree.component';
+import { DetailComponent } from '../../kb/component/detail/detail.component';
+import { DetailInfoComponent } from '../../kb/component/detail-info/detail-info.component';
+import { MainDocumentComponent } from '../../kb/component/main-document/main-document.component';
+import { DocumentComponent } from '../../kb/component/document/document.component';
+import { KeywordComponent } from '../../kb/component/keyword/keyword.component';
+import { KbDetailComponent } from './kb-detail/kb-detail.component';
+import { KbDetailInfoComponent } from './kb-detail-info/kb-detail-info.component';
+import { Subscription } from 'rxjs';
+import { KbStore } from '../../kb/kb.store';
+import { KbService } from '../../kb/kb.service';
+import { CasekbService } from '../../case/casekb/casekb.service';
+import Utils from 'src/app/shared/utils';
+import { TabParam } from 'src/app/layouts/admin/tab-manage.service';
+
+@Component({
+  selector: 'app-modal-kb',
+  standalone: true,
+  imports: [SharedModule, TreeComponent, KbDetailComponent, KbDetailInfoComponent, DetailComponent, DetailInfoComponent, MainDocumentComponent, DocumentComponent, KeywordComponent],
+  templateUrl: './modal-kb.component.html',
+  styleUrl: './modal-kb.component.scss'
+})
+export class ModalKbComponent extends BaseComponent implements OnInit {
+  kbDetailSubscription: Subscription;
+  kbTreeSubscription: Subscription;
+  kbDetail: any = null;
+  createForm: FormGroup;
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private dialogRef: MatDialogRef<ModalKbComponent>,
+    public api: ApiService,
+    private formBuilder: FormBuilder,
+    public router: Router,
+    public globals: Globals,
+    private kbStore: KbStore,
+    private kbService: KbService,
+    private caseKbService: CasekbService
+  ) {
+    super(router, globals);
+    this.initCreateForm();
+    // this.kbTreeSubscription = this.kbStore.observeKbContentType().subscribe(contentType => {
+    //   if (contentType) {
+    //     this.loadKbTree();
+    //   }
+    // });
+
+    this.kbDetailSubscription = this.kbStore.observeKbDetail().subscribe(detail => {
+      if (detail && detail.contentId) {
+        this.kbService.getKbDetailInfoById({
+          data: detail.contentId
+        }).then(result => {
+          if (result.status) {
+            this.createForm.patchValue({ kbId: result.data.contentId, caseNumber: this.data.caseNumber });
+          }
+        });
+      } else {
+        this.createForm.patchValue({ kbId: null, caseNumber: null });
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.kbStore.loadKbContentType("Content");
+  }
+
+  close(isSuccess: boolean): void {
+    // ส่งค่ากลับไปยัง component ที่เปิด modal
+    this.dialogRef.close(isSuccess);
+  }
+
+  initCreateForm() {
+    this.createForm = this.formBuilder.group({
+      caseNumber: [''],
+      kbId: [null]
+    });
+  }
+
+
+  onCreateRefKB() {
+    // alert(JSON.stringify(this.createForm.getRawValue()));
+    const param = this.createForm.getRawValue();
+    this.caseKbService.createRefKB({ data: { ...param } }).then((res) => {
+      if (res.status) {
+        this.close(true);
+      } else {
+        Utils.alertError({
+          text: res.message
+        });
+        this.close(false)
+      }
+    })
+  }
+
+}
