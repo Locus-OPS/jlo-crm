9a12155f2767b864779aa1a66adba05cf4ee72e7
apichat <apichathot@gmail.com>
2024-09-18 14:55:45 +0700

    email inbound detail

--- src/app/pages/email-inbound/email-inbound-detail/email-inbound-detail.component.ts
+++ src/app/pages/email-inbound/email-inbound-detail/email-inbound-detail.component.ts
@@ -2,12 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { SharedModule } from 'src/app/shared/module/shared.module';
 import { CreatedByComponent } from '../../common/created-by/created-by.component';
 import { BaseComponent } from 'src/app/shared/base.component';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { EmailInboundService } from '../email-inbound.service';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { Globals } from 'src/app/shared/globals';
 import { ApiService } from 'src/app/services/api.service';
+import { TabParam } from 'src/app/layouts/admin/tab-manage.service';
+import Utils from 'src/app/shared/utils';
+import Quill from 'quill';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { ModalContentFileComponent } from '../../common/modal-file/modal-file.component';
 
 @Component({
   selector: 'app-email-inbound-detail',
@@ -18,6 +23,16 @@ import { ApiService } from 'src/app/services/api.service';
 })
 export class EmailInboundDetailComponent extends BaseComponent implements OnInit {
 
+  createForm: FormGroup;
+  modules: any = {};
+  attItemsList: any[] = [];
+
+  fileName: string;
+  fileType: string;
+  fileTempUrl: string;
+  fileUrl: SafeResourceUrl;
+  downloadfileUrl: SafeResourceUrl;
+
   constructor(
     private formBuilder: FormBuilder,
     private emailIbService: EmailInboundService,
@@ -25,22 +40,81 @@ export class EmailInboundDetailComponent extends BaseComponent implements OnInit
     public router: Router,
     public globals: Globals,
     public api: ApiService,
+    private tabParam: TabParam,
+    public sanitizer: DomSanitizer,
   ) {
     super(router, globals);
-    api.getMultipleCodebookByCodeType({
-      data: ['ACTIVE_FLAG']
-    }).then(
-      result => {
-
-
-      }
-    );
-
-
   }
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+
+    this.createForm = this.formBuilder.group({
+      id: [''],
+      formEmail: [''],
+      toEmail: [''],
+      subjectEmail: [''],
+      plainContent: [""],
+      htmlContent: [""],
+      createdByName: [''],
+      createdBy: [''],
+      createdDate: [''],
+      updatedBy: [''],
+      updatedByName: [''],
+      updatedDate: ['']
+    });
+
+    this.modules = {
+      toolbar: { container: [] },
+    };
+
+    this.getEmailInboundDetail();
+
+    this.getEmailInboundAttListById();
+  }
+
+
+  getEmailInboundDetail() {
+    if (this.tabParam.params['id']) {
+      console.log("this.tabParam.params['id'] " + this.tabParam.params['id']);
+
+      this.createForm.patchValue({ id: this.tabParam.params['id'] });
+
+      this.emailIbService.getEmailInboundDetailById({ data: { id: this.tabParam.params['id'] } })
+        .then(result => {
+          this.createForm.patchValue(result.data);
+        }, error => {
+          Utils.alertError({
+            text: 'Please try again later.',
+          });
+        });
+    }
+  }
+
+
+  getEmailInboundAttListById() {
+
+    if (this.tabParam.params['id']) {
+      this.emailIbService.getEmailInboundAttListById({ data: { id: this.tabParam.params['id'] } }).then(res => {
+        console.log(res.data);
+        this.attItemsList = res.data;
+
+
+      });
+    }
+  }
+
+
+  openAttachmentDialog(attId: number, fileExtension: string, fileName: string) {
+    const dialogRef = this.dialog.open(ModalContentFileComponent, {
+      data: {
+        attId: attId,
+        fileExtension: fileExtension,
+        fileName: fileName
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(() => {
+    });
   }
 
 }
