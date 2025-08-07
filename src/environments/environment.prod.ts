c5b714a7cdc1c0cd143f2a1060873ebadf93b1f9
Apichat <apichathot@gmail.com>
2025-08-07 15:25:22 +0700

    Upgrade bootstrap 4.x.x To 5.3.7

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   endpoint: 'http://52.221.33.21/jlo-crm-backend',
-  endpointWebsocket: 'wss://52.221.33.21/jlo-crm-ws/chat',
-  whitelistedDomains: ['jlo.locus.co.th']
+  endpointWebsocket: 'ws://52.221.33.21/jlo-crm-ws/chat',
+  whitelistedDomains: ['52.221.33.21']
 };
